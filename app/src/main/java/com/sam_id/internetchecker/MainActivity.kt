fd96aaf36b7f5c760579baf2b00c3a7fd3c5901f
Dimas Prasetya <prasetya.dimas95@gmail.com>
2020-07-03 23:05:32 +0700

    add network connection class to get connection status

--- app/src/main/java/com/sam_id/internetchecker/MainActivity.kt
+++ app/src/main/java/com/sam_id/internetchecker/MainActivity.kt
@@ -2,10 +2,24 @@ package com.sam_id.internetchecker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val networkConnection = NetworkConnection(applicationContext)
+        networkConnection.observe(this, Observer { isConnected ->
+            if (isConnected) {
+                disconnected_layout.visibility = View.GONE
+                connected_layout.visibility = View.VISIBLE
+            } else {
+                connected_layout.visibility = View.GONE
+                disconnected_layout.visibility = View.VISIBLE
+            }
+        })
     }
 }
